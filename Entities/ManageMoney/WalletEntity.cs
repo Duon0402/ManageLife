8e16089066bd2c14df33cb463047fe8e07373e38
DuongDT <duongdangtruong.it@gmail.com>
2024-12-21 21:48:55 +0700

    [Base] - Thêm các folder và file base

--- Entities/ManageMoney/WalletEntity.cs
+++ Entities/ManageMoney/WalletEntity.cs
@@ -7,13 +7,10 @@ namespace ManageLife.Entities
         public string Name { get; set; }
         public string Type { get; set; }
         public decimal TotalMoney { get; set; }
-
         public DateTime CreatedTime { get; set; }
         public string CreatedUser { get; set; }
-
         public DateTime UpdatedTime { get; set; }
         public string UpdatedUser { get; set; }
-
         public bool IsDeleted { get; set; }
         public string DeletedUser { get; set; }
         public DateTime DeletedTime { get; set; }
