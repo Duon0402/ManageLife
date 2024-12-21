b3130d84bc1a948642964b71f54411ef8ff2655e
DuongDT <duongdangtruong.it@gmail.com>
2024-12-21 22:51:42 +0700

    [Base] - Thêm mới ResultBase

--- Base/ResultBase/Result.cs
+++ Base/ResultBase/Result.cs
@@ -1,7 +1,57 @@
 ﻿namespace ManageLife.Base
 {
-    public static class Result
+    public class Result
     {
-        
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public string? ErrorContent { get; set; }
+
+        public Result(string code, string message, string? errorContent = null)
+        {
+            Code = code;
+            Message = message;
+            ErrorContent = errorContent;
+        }
+
+        public static Result Ok()
+        {
+            return new Result("00", "Ok");
+        }
+
+        public static Result Error(string code, string message, string? errorContent = null)
+        {
+            return new Result(code, message, errorContent);
+        }
+
+        public static Result Exception(string msg, Exception ex)
+        {
+            return new Result("99", msg, ex.ToString());
+        }
+
+        public bool IsOk()
+        {
+            return Code == "00";
+        }
+
+        public static Result<T> Ok<T>(T data)
+        {
+            return new Result<T>("00", "Ok", data);
+        }
+
+        public static Result<T> Error<T>(string code, string message, string? errorContent = null)
+        {
+            return new Result<T>(code, message, default, errorContent);
+        }
+
+        public static Result<T> Exception<T>(string message, Exception ex)
+        {
+            return new Result<T>("99", message, default, ex.ToString());
+        }
+
+        public static readonly Result DATA_CAN_NOT_CREATE = new Result("01", "Dữ liệu không thể thêm mới");
+        public static readonly Result DATA_CAN_NOT_UPDATE = new Result("02", "Dữ liệu không thêm chỉnh sửa");
+        public static readonly Result DATA_CAN_NOT_DELETE = new Result("03", "Dữ liệu không thể xóa");
+        public static readonly Result DATA_EXIST = new Result("04", "Dữ liệu đã tồn tại");
+        public static readonly Result DATA_NOT_EXIST = new Result("05", "Dữ liệu không tồn tại");
     }
 }
---
+++ Base/ResultBase/ResultWithData.cs
@@ -0,0 +1,28 @@
+﻿namespace ManageLife.Base
+{
+    public class Result<T> : Result
+    {
+        public T? Data { get; set; }
+
+        public Result(string code, string msg, T? data = default, string? errorContent = null)
+            : base(code, msg, errorContent)
+        {
+            Data = data;
+        }
+
+        public Result<T> Ok(T data)
+        {
+            return new Result<T>("00", "Ok", data);
+        }
+
+        public new Result<T> Error(string code, string message, string? errorContent = null)
+        {
+            return new Result<T>(code, message, default, errorContent);
+        }
+
+        public new Result<T> Exception(string message, Exception ex)
+        {
+            return new Result<T>("99", message, default, ex.ToString());
+        }
+    }
+}
