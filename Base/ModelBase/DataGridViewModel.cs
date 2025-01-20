ddd35b021be9cfe8fbee4734ca7709ba233ea272
DuongDT <duongdangtruong.it@gmail.com>
2025-01-20 23:49:45 +0700

    [View] - Taoj GridData Base

---
+++ Base/ModelBase/DataGridViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ManageLife.Base
+{
+    public class DataGridViewModel<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public string[] ColumnNames { get; set; }
+        public List<DataGridAction> Actions { get; set; } = new List<DataGridAction>();
+    }
+
+    public class DataGridAction
+    {
+        public string ButtonText { get; set; }
+        public string CssClass { get; set; }
+        public string OnClickFunction { get; set; }
+    }
+}
