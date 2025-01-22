b3953e84cd288994d101480507e58d786b86b7b0
DuongDT <duongdt@hncjsc.vn>
2025-01-22 11:04:58 +0700

    [Base-View] - Thêm formview (commit tạm)

---
+++ Base/ModelBase/FormViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace ManageLife.Base
+{
+    public class FormViewModel
+    {
+        public FormViewModel()
+        {
+            FormModels = new List<FormModel<object>>();
+        }
+
+        public List<FormModel<object>> FormModels { get; set; }
+    }
+
+    public class FormModel<T>
+    {
+        public string FieldName { get; set; }
+        public T? FieldValue { get; set; }
+        public string FieldType { get; set; }
+    }
+
+    public class FormOptions
+    {
+
+    }
+}
