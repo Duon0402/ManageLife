678786cda35154b32bb595b508290ee247bf8f68
DuongDT <duongdt@hncjsc.vn>
2025-04-18 09:55:45 +0700

    [Project] Cập nhật thêm cho MenuRegister

--- Helpers/MenuRegister/MenuRegister.cs
+++ Helpers/MenuRegister/MenuRegister.cs
@@ -2,19 +2,19 @@
 
 namespace ManageLife.Helpers
 {
-	public class MenuRegister : IMenuRegister
-	{
-		public List<MenuItem> GetListMenuItems()
-		{
-			var listMenuItems = new List<MenuItem>();
+    public class MenuRegister : IMenuRegister
+    {
+        public List<MenuItem> GetListMenuItems()
+        {
+            var listMenuItems = new List<MenuItem>();
 
-			listMenuItems.AddRange(new List<MenuItem>
-			{
-				new MenuItem("Dashboard", "/Dashboard", "fa-solid fa-house"),
-				new MenuItem("Users", "/Users", "fa-solid fa-users", new List<MenuItem>())
-			});
+            listMenuItems.AddRange(new List<MenuItem>
+            {
+                new MenuItem("Dashboard", "/Dashboard", "fa-solid fa-house"),
+                new MenuItem("Users", "fa-solid fa-users", new List<MenuItem>())
+            });
 
-			return listMenuItems;
-		}
-	}
+            return listMenuItems;
+        }
+    }
 }
