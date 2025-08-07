b4892b35e7453af14170081dafc3f1d2339a481f
DuongDT <duongdt@hncjsc.vn>
2025-08-07 11:03:22 +0700

    [Helper] Thêm mới FileUploader - Commit tạm

---
+++ Base/OptionBase/FileUploaderOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ManageLife.Base
+{
+    public class FileUploaderOptions
+    {
+        public string? Id { get; set; }
+        public string? Width { get; set; }
+        public int FileCount
+    }
+}
---
+++ Helpers/HtmlHelper/FileUploaderHelper.cs
@@ -0,0 +1,22 @@
+﻿using ManageLife.Base;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManageLife.Helpers
+{
+    public static class FileUploaderHelper
+    {
+        public static IHtmlContent FileUploader(this IHtmlHelper htmlHelper, FileUploaderOptions options)
+        {
+
+            var fileUploaderBuilder = new TagBuilder("div");
+            fileUploaderBuilder.AddCssClass("file-uploader");
+            if (options.Id.IsNotEmpty())
+            {
+                fileUploaderBuilder.Attributes.Add("id", options.Id);
+            }
+
+            return fileUploaderBuilder;
+        }
+    }
+}
