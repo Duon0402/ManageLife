2811a1b98aa83230d32c0c45d7a5fc4e6d520e2d
DuongDT <duongdt@hncjsc.vn>
2025-08-12 10:08:54 +0700

    Commit tạm phần FileUploadHelper

--- Base/ExtentionBase/CollectionExtention.cs
+++ Base/ExtentionBase/CollectionExtention.cs
@@ -1,26 +1,29 @@
-﻿namespace ManageLife.Base
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ManageLife.Base
 {
-	//TODO: Chuyển thành Extention Method
-	public static class CollectionExtention
-	{
-		public static bool IsEmpty<T>(this ICollection<T> collection)
-		{
-			return collection == null || collection.Count == 0;
-		}
+    public static class CollectionExtention
+    {
+        public static bool IsEmpty<T>(this ICollection<T>? collection)
+        {
+            return collection == null || collection.Count == 0;
+        }
 
-		public static bool IsNotEmpty<T>(this ICollection<T> collection)
-		{
-			return !collection.IsEmpty();
-		}
+        public static bool IsNotEmpty<T>(
+            [NotNullWhen(true)] this ICollection<T>? collection)
+        {
+            return collection != null && collection.Count > 0;
+        }
 
-		public static bool IsEmpty<T>(this IEnumerable<T> collection)
-		{
-			return collection == null || !collection.Any();
-		}
+        public static bool IsEmpty<T>(this IEnumerable<T>? collection)
+        {
+            return collection == null || !collection.Any();
+        }
 
-		public static bool IsNotEmpty<T>(this IEnumerable<T> collection)
-		{
-			return !collection.IsEmpty();
-		}
-	}
+        public static bool IsNotEmpty<T>(
+            [NotNullWhen(true)] this IEnumerable<T>? collection)
+        {
+            return collection != null && collection.Any();
+        }
+    }
 }
--- Helpers/HtmlHelper/FileUploaderHelper.cs
+++ Helpers/HtmlHelper/FileUploaderHelper.cs
@@ -1,18 +1,16 @@
 ﻿using ManageLife.Base;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Json;
 
 namespace ManageLife.Helpers
 {
     public static class FileUploaderHelper
     {
-        #region Render HTML
         public static IHtmlContent FileUploader(this IHtmlHelper htmlHelper, FileUploaderOptions options)
         {
             if (options.Id.IsEmpty())
-            {
                 options.Id = IdHeper.NewId();
-            }
 
             var container = CreateContainer(options);
             container.InnerHtml.AppendHtml(CreateHeader(options));
@@ -27,6 +25,8 @@ namespace ManageLife.Helpers
             return wrapper;
         }
 
+        #region HTML Render
+
         private static TagBuilder CreateContainer(FileUploaderOptions options)
         {
             var container = new TagBuilder("div");
@@ -34,14 +34,10 @@ namespace ManageLife.Helpers
             container.Attributes["style"] = $"width: {options.Width};";
 
             if (options.CssClass.IsNotEmpty())
-            {
                 container.AddCssClass(options.CssClass!);
-            }
 
             if (options.Id.IsNotEmpty())
-            {
                 container.Attributes["id"] = options.Id;
-            }
 
             return container;
         }
@@ -67,7 +63,7 @@ namespace ManageLife.Helpers
         {
             var toolbar = new TagBuilder("div");
             toolbar.AddCssClass("uploader-toolbar");
-            toolbar.Attributes["style"] = $"display: none;";
+            toolbar.Attributes["style"] = "display: none;";
 
             if (options.UploadMode == UploadMode.OnButtonClick)
             {
@@ -122,9 +118,7 @@ namespace ManageLife.Helpers
             fileBrowseInput.Attributes.Add("type", "file");
 
             if (options.AllowMultiFileUpload)
-            {
                 fileBrowseInput.Attributes.Add("multiple", "multiple");
-            }
 
             fileBrowseInput.Attributes.Add("accept", options.Accept);
             fileBrowseInput.Attributes.Add("hidden", "hidden");
@@ -132,40 +126,206 @@ namespace ManageLife.Helpers
 
             return fileUploadBox;
         }
+
         #endregion
 
-        #region Render JS
+        #region Script Render
+
         private static TagBuilder CreateScript(FileUploaderOptions options)
         {
             var script = new TagBuilder("script");
             script.Attributes["type"] = "text/javascript";
 
-            var js = "";
+            var id = options.Id ?? "fileUploader";
+            var uploadUrl = options.UploadUrl;
+            var maxFileCount = options.MaxFileCount ?? 0;
+            var maxFileSize = options.MaxFileSize ?? 0;
+            var uploadMode = options.UploadMode.ToString();
+            var allowRemove = options.AllowRemove ? "true" : "false";
+            var allowMultiFileUpload = options.AllowMultiFileUpload ? "true" : "false";
+
+            var additionalDataJson = options.AdditionalData.IsNotEmpty()
+                ? JsonSerializer.Serialize(options.AdditionalData)
+                : "{}";
+
+            var js = $@"
+                (function(){{
+                    {GenerateVariables(id)}
+                    {GenerateUtils(maxFileCount, maxFileSize, allowRemove)}
+                    {GenerateUploadFunction(uploadUrl, additionalDataJson)}
+                    {GenerateHandleFiles(uploadMode, allowRemove)}
+                    {GenerateEvents(uploadMode)}
+                    {GenerateInit()}
+                }})();
+                ";
 
             script.InnerHtml.AppendHtml(js);
             return script;
         }
 
+        private static string GenerateVariables(string id) => $@"
+            var container = $('#{id}');
+            var fileList = container.find('.file-list');
+            var fileBrowseButton = container.find('.file-browse-button');
+            var fileBrowseInput = container.find('.file-browse-input');
+            var fileUploadBox = container.find('.file-upload-box');
+            var statusText = container.find('.file-completed-status');
+            var toolbar = container.find('.uploader-toolbar');
 
-        private static string GenerateVariables(FileUploaderOptions options)
-        {
-            var id = options.Id ?? "fileUploader";
+            var completedCount = 0;
+            var totalCount = 0;
+            var fileQueue = [];
+        ";
 
-            return $@"
-                var container = $('#{id}');
-                var fileList = container.find('.file-list');
-                var fileBrowseButton = container.find('.file-browse-button');
-                var fileBrowseInput = container.find('.file-browse-input');
-                var fileUploadBox = container.find('.file-upload-box');
-            ";
-        }
+        private static string GenerateUtils(int maxFileCount, long maxFileSize, string allowRemove) => $@"
+            function updateStatus(){{
+                statusText.text(completedCount + ' / ' + totalCount + ' Files Completed');
+            }}
 
-        private static string GenerateDragDropScript(FileUploaderOptions options)
-        {
-            var id = options.Id ?? "fileUploader";
+            function createFileItemHtml(file){{
+                var ext = file.name.split('.').pop();
+                var sizeMb = (file.size / (1024*1024)).toFixed(2);
+                return `
+                    <li class='file-item'>
+                        <div class='file-extension'>${{ext}}</div>
+                        <div class='file-content-wrapper'>
+                            <div class='file-content'>
+                                <div class='file-details'>
+                                    <h5 class='file-name'>${{file.name}}</h5>
+                                    <div class='file-info'>
+                                        <small class='file-size'>${{sizeMb}} MB</small>
+                                        <small class='file-divider'>-</small>
+                                        <small class='file-status'>Pending</small>
+                                    </div>
+                                </div>
+                                {(allowRemove == "true" ? "<button class='cancel-button'><i class='fa-solid fa-xmark'></i></button>" : "")}
+                            </div>
+                            <div class='file-progress-bar'>
+                                <div class='file-progress'></div>
+                            </div>
+                        </div>
+                    </li>
+                `;
+            }}
 
-            return string.Empty;
-        }
+            function validateFile(file){{
+                if ({maxFileCount} > 0 && (totalCount + 1) > {maxFileCount}) {{
+                    alert('Max file count is {maxFileCount}');
+                    return false;
+                }}
+                if ({maxFileSize} > 0 && file.size > {maxFileSize}) {{
+                    alert('File ' + file.name + ' exceeds max size');
+                    return false;
+                }}
+                return true;
+            }}
+        ";
+
+        private static string GenerateUploadFunction(string uploadUrl, string additionalDataJson) => $@"
+            function uploadFile(file, itemEl){{
+                var formData = new FormData();
+                formData.append('file', file);
+
+                var extraData = {additionalDataJson};
+                for (var key in extraData){{
+                    formData.append(key, extraData[key]);
+                }}
+
+                ajaxService.upload('{uploadUrl}', formData, {{
+                    showLoading: false,
+                    hideLoading: false,
+                    onProgress: function(percent){{
+                        itemEl.find('.file-progress').css('width', percent + '%');
+                        itemEl.find('.file-status').text('Uploading... ' + percent + '%');
+                    }},
+                    onSuccess: function(res){{
+                        completedCount++;
+                        updateStatus();
+                        itemEl.find('.file-status').text('Completed');
+                        itemEl.find('.file-progress').css('width', '100%');
+                    }},
+                    onError: function(err){{
+                        completedCount++;
+                        updateStatus();
+                        itemEl.find('.file-status').text('Failed');
+                        itemEl.find('.file-progress').css('background', 'red');
+                    }}
+                }});
+            }}
+        ";
+
+        private static string GenerateHandleFiles(string uploadMode, string allowRemove) => $@"
+            function handleSelectedFiles(files){{
+                if (!files || files.length === 0) return;
+                [...files].forEach(function(file){{
+                    if (!validateFile(file)) return;
+
+                    totalCount++;
+                    updateStatus();
+
+                    var $item = $(createFileItemHtml(file));
+                    fileList.append($item);
+
+                    if ('{uploadMode}' === 'Instant'){{
+                        uploadFile(file, $item);
+                    }} else {{
+                        fileQueue.push({{file:file, el:$item}});
+                        toolbar.show();
+                    }}
+
+                    if ({allowRemove}) {{
+                        $item.find('.cancel-button').on('click', function(){{
+                            $item.remove();
+                            totalCount--;
+                            updateStatus();
+                        }});
+                    }}
+                }});
+            }}
+        ";
+
+        private static string GenerateEvents(string uploadMode) => $@"
+            fileUploadBox.on('drop', function(e){{
+                e.preventDefault();
+                handleSelectedFiles(e.originalEvent.dataTransfer.files);
+                fileUploadBox.removeClass('active');
+            }}).on('dragover', function(e){{
+                e.preventDefault();
+                fileUploadBox.addClass('active');
+            }}).on('dragleave', function(e){{
+                e.preventDefault();
+                fileUploadBox.removeClass('active');
+            }});
+
+            fileBrowseButton.on('click', function(){{
+                fileBrowseInput.click();
+            }});
+
+            fileBrowseInput.on('change', function(e){{
+                handleSelectedFiles(e.target.files);
+                fileBrowseInput.val('');
+            }});
+
+            container.find('.upload-all-button').on('click', function(){{
+                fileQueue.forEach(function(item){{
+                    uploadFile(item.file, item.el);
+                }});
+                fileQueue = [];
+                toolbar.hide();
+            }});
+
+            container.find('.remove-all-button').on('click', function(){{
+                fileList.empty();
+                totalCount = 0;
+                completedCount = 0;
+                updateStatus();
+                fileQueue = [];
+                toolbar.hide();
+            }});
+        ";
+
+        private static string GenerateInit() => "updateStatus();";
+
         #endregion
     }
 }
