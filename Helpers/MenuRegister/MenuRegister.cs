7acd9da8ad695f533885be058640d1fe7efa7e95
DuongDT <duongdt@hncjsc.vn>
2025-08-18 11:24:02 +0700

    Sửa lỗi không load appsettings

--- Helpers/MenuRegister/MenuRegister.cs
+++ Helpers/MenuRegister/MenuRegister.cs
@@ -8,12 +8,7 @@ namespace ManageLife.Helpers
         {
             var listMenuItems = new List<MenuItem>
             {
-                new MenuItem("Dashboard", "/Dashboard", "fa-solid fa-house"),
-                new MenuItem("Users", icon: "fa-solid fa-users", subItems: new List<MenuItem>
-                {
-                    new MenuItem("User List", "/Users", "fa-solid fa-list"),
-                    new MenuItem("Create User", "/Users/Create", "fa-solid fa-plus")
-                })
+                new MenuItem("Dashboard", "/Admin/Dashboard", "fa-solid fa-house")
             };
 
             return listMenuItems;
