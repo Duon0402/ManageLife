00732ee05fc984e9eb81645f3022111acd90e1c2
DuongDT <duongdt@hncjsc.vn>
2025-11-28 14:37:38 +0700

    Sửa lại phần lấy ngày trong gen mail utility

--- Services/UtilityService.cs
+++ Services/UtilityService.cs
@@ -26,7 +26,11 @@ namespace ManageLife.Services
                 }
 
                 var today = DateTimeHelper.VNTime().Date;
-                var tomorrow = today.AddDays(1);
+
+                var nextBusinessDate = today.DayOfWeek == DayOfWeek.Saturday
+                    ? today.AddDays(2)
+                    : today.AddDays(1);
+
                 var fullName = "Đặng Trường Dương";
                 var employeeCode = "002740";
                 var department = "Tổ Web - Phòng PTPM";
@@ -36,7 +40,7 @@ namespace ManageLife.Services
                     EmailDailyReportTemplate.Subject,
                     fullName,
                     today.ToString("dd.MM.yyyy"),
-                    tomorrow.ToString("dd.MM.yyyy")
+                    nextBusinessDate.ToString("dd.MM.yyyy")
                 );
 
                 var body = string.Format(
@@ -46,7 +50,7 @@ namespace ManageLife.Services
                     department,
                     supervisor,
                     today.ToString("dd.MM.yyyy"),
-                    tomorrow.ToString("dd.MM.yyyy"),
+                    nextBusinessDate.ToString("dd.MM.yyyy"),
                     request.TodayResult,
                     request.TomorrowPlan,
                     request.Suggestion.IsNotEmpty() ? request.Suggestion : "- Không có"
