3cc2547819a69662975d4d7417a9ba662b2f5f1f
DuongDT <duongdt@hncjsc.vn>
2025-12-22 10:16:27 +0700

    Sửa ITokenSerivce

--- Interfaces/ITokenService.cs
+++ Interfaces/ITokenService.cs
@@ -6,7 +6,7 @@ namespace ManageLife.Interfaces
 {
     public interface ITokenService
     {
-        public string GenerateAccessToken(string userId, string username, IEnumerable<string> roles);
+        public string GenerateAccessToken(string userId, string username, string securityStamp, IEnumerable<string> roles);
         public string GenerateRefreshToken();
         public ClaimsPrincipal? ValidateAccessToken(string? token);
         public void SetTokensCookie(string accessToken, string refreshToken);
